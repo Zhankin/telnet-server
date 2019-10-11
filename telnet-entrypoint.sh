d1a0bfe60c29f98b2cce4ee1c653369b6194cc26
Zhankin <zhankin@mail.ru>
2019-10-11 18:59:56 +0600

    Update telnet-entrypoint.sh

--- telnet-entrypoint.sh
+++ telnet-entrypoint.sh
@@ -3,4 +3,6 @@ if [ "$USERPASS" = "**String**" ]; then
     export USERPASS='production'
 fi
 echo root:${USERPASS} | chpasswd
+
 bash -c xinetd -dontfork -stayalive
+exec /bin/bash -ec "while :; do echo '.'; sleep 5 ; done"
